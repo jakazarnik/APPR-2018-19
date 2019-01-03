790d8a99819c0c2c9afb050049d11821a2954b72
Jakob <jaka,zarnik@gmail.com>
2019-01-03 11:32:14 +0100

    naredu sem markdown file

--- lib/libraries.r
+++ lib/libraries.r
@@ -4,7 +4,9 @@ library(gsubfn)
 library(reshape2)
 library(shiny)
 library(readr)
-
+library(dplyr)
+library(rmarkdown)
+library(tidyr)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r")
