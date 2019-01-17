4d94b9fdf4b6a50401e4c059b9d57f9a0fd6b4de
Jakob <jaka,zarnik@gmail.com>
2019-01-17 08:47:56 +0100

    rmd zdej dela

--- vizualizacija/vizualizacija_moja.R
+++ vizualizacija/vizualizacija_moja.R
@@ -18,9 +18,9 @@ ggplot(Slovenija, aes(x=long, y=lat, group=group, fill=NAME_1)) +
 
 #GRAFI
 
-gibanje_prebivalstva <- ggplot(data = gibanje_celotnega_prebivalstva, mapping = aes(x=leto, y=prebivalstvo_1_januar)) + geom_line()
-plot(gibanje_prebivalstva)
-                    
-povrsine_cake <- ggplot(data = povrsine, mapping = aes(x="", y=povrsina_km2, fill=regija)) + geom_bar(width = 1, stat = "identity")
-povrsine_cake <- povrsine_cake + coord_polar(theta = "y") + theme_void()
-plot(povrsine_cake)
+# gibanje_prebivalstva <- ggplot(data = gibanje_celotnega_prebivalstva, mapping = aes(x=leto, y=prebivalstvo_1_januar)) + geom_line()
+# plot(gibanje_prebivalstva)
+#                     
+# povrsine_cake <- ggplot(data = povrsine, mapping = aes(x="", y=povrsina_km2, fill=regija)) + geom_bar(width = 1, stat = "identity")
+# povrsine_cake <- povrsine_cake + coord_polar(theta = "y") + theme_void()
+# plot(povrsine_cake)
