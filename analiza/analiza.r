c40694db0fa909118f251648bf87eadbc5e3dd7e
Jakob <jaka,zarnik@gmail.com>
2019-05-17 18:01:12 +0200

    analiza update

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -16,16 +16,16 @@ gostota_graf2 <- function(cifra) {
 
 fit <- lm(data = tabela_mladi, stevilo ~ leto)
 
-cas <- data.frame(leto=seq(2016, 2030, 1))
+cas <- data.frame(leto=seq(1995, 2030, 2))
 
-predict(fit, a)
+predict(fit, cas)
 
-napoved <- a %>% mutate(stevilo=predict(fit, .))
+napoved <- cas %>% mutate(stevilo=predict(fit, cas))
 
 mladi_graf3 <- ggplot(napoved, aes(x=leto, y=stevilo)) +
   scale_y_continuous(limits = c(30000, 50000)) +
   geom_smooth(method=lm, se=FALSE, fullrange = TRUE) +
-  geom_point(data=napoved, aes(x=leto, y=leto), color="red", size=3) +
+  geom_point(data=napoved, color="red", size=2) +
   labs(title="Napoved števila mladih", y="število prebivalcev 0-14let") + 
   geom_point()
 
