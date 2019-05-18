a485c71984e4d71d7ff4238f150b6487a3fc2ca3
Jakob <jaka,zarnik@gmail.com>
2019-05-18 10:29:04 +0200

    popravlje aproksimacija

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -14,18 +14,17 @@ gostota_graf2 <- function(cifra) {
 
 #Napredna analiza - napoved stevila mladih
 
-fit <- lm(data = tabela_mladi, stevilo ~ leto)
+fit <- lm(data = tabela_mladi, stevilo ~ poly(leto, 6, raw=TRUE))
 
-cas <- data.frame(leto=seq(1995, 2030, 2))
+cas <- data.frame(leto=seq(1995, 2025, 2))
 
 predict(fit, cas)
 
 napoved <- cas %>% mutate(stevilo=predict(fit, cas))
 
-mladi_graf3 <- ggplot(napoved, aes(x=leto, y=stevilo)) +
-  geom_smooth(method=lm, se=FALSE, fullrange = TRUE) +
-  geom_point(data=napoved, color="red", size=3) +
-  labs(title="Napoved števila mladih", y="število prebivalcev 0-14let") + 
+mladi_graf3 <- ggplot(tabela_mladi, aes(x=leto, y=stevilo)) +
+  geom_smooth(data=napoved, method = "lm", formula = y ~ poly(x, 6), fullrange = TRUE) +
+  labs(title="Napoved števila mlajšega prebivalstva", y="število prebivalcev 0-14let") + 
   geom_point()
 
 plot(mladi_graf3)
